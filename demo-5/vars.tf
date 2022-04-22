ca86b9dc0424b7a2e4a21b4e3aceffae4ef4e50c
João Melo <jopcmelo@gmail.com>
2022-04-22 12:38:55 -0300

    feat: create a security group using datasource to allow all european inbound traffic

--- demo-5/vars.tf
+++ demo-5/vars.tf
@@ -1,11 +1,11 @@
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
+    us-east-1 = "ami-0b0ea68c435eb488d"
     us-west-2 = "ami-06b94666"
     eu-west-1 = "ami-844e0bf7"
   }
